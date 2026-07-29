fa9598c65680b2c7ad47d92c98d2dcf0f537f5c7
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-07-29 19:43:20 +0900

    initialize as node project to use eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+      },
+    },
+  },
+  { ignores: ["**/jquery*.min.js"] },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "arrow-body-style": ["warn", "as-needed"],
+    },
+  },
+]);
